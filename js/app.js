edeadf742df40829b236c262ed1aa5f54c8fb683
NGUYEN Thi Thuy Dung <60779104+thuydung23@users.noreply.github.com>
2020-05-26 11:32:56 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,5 +25,45 @@ function active_menu(){
         }
     });
 }
+function dowload(fileURL,fileName){
+    $(document).ready(function(){
+        $("#dowload").click(function(){
+            if (!window.ActiveXObject) {
+                var save = document.createElement('a');
+                save.href = fileURL;
+                save.target = '_blank';
+                console.log(fileURL)
+                var filename = fileURL.substring(fileURL.lastIndexOf('/')+1);
+                save.download = fileName || filename;
+                   if ( navigator.userAgent.toLowerCase().match(/(ipad|iphone|safari)/) && navigator.userAgent.search("Chrome") < 0) {
+                        document.location = save.href; 
+                    // window event not working here
+                    }else{
+                        var evt = new MouseEvent('click', {
+                            'view': window,
+                            'bubbles': true,
+                            'cancelable': false
+                        });
+                        save.dispatchEvent(evt);
+                        (window.URL || window.webkitURL).revokeObjectURL(save.href);
+                    }	
+            }
+        
+            // for IE < 11
+            else if ( !! window.ActiveXObject && document.execCommand)     {
+                var _window = window.open(fileURL, '_blank');
+                _window.document.close();
+                _window.document.execCommand('SaveAs', true, fileName || fileURL)
+                _window.close();
+            }
+        });
+    });
+}
 
 active_menu();
+
+var url = "NGUYEN_Thi_Thuy_Dung-CV.pdf";
+var fileName = "NGUYEN_Thi_Thuy_Dung-CV";
+
+
+
