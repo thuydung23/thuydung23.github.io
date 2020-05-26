561aaf497b91754d2698fc90e62a20ffffc3af6c
NGUYEN Thi Thuy Dung <60779104+thuydung23@users.noreply.github.com>
2020-05-26 11:39:23 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
+function active_menu(){
+    $(document).ready(function(){
 
+        var array = ['user','formations','projets','informations','competences'];
+
+        for(var i=0; i<array.length; i++){
+            var tmp = '#'+array[i];
+            $(tmp).click(function(){
+                if($(".active").length !=0){
+                    var item = $(".active");
+                    item.addClass('none-active');
+                    item.removeClass('active');
+                }
+                
+                if($(this).attr('id') != 'user'){
+                    var url = "templates/"+$(this).attr('id')+".html";
+                    $(this).addClass('active');
+                    $(this).removeClass('none-active');
+                }else{
+                    var url = "templates/description.html";
+                }
+                
+                $("#iframe").attr("src",url);
+            });
+        }
+    });
+}
 function dowload(fileURL,fileName){
     $(document).ready(function(){
         $("#dowload").click(function(){
